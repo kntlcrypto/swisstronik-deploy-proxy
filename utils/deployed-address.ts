6776bb12046a35aff67c72e9ad49acc52ba181a3
captdavid99 <captdavid99@outlook.com>
2024-08-19 19:48:32 +0700

    initial commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x2F96F30E6712a5342AAe113B728455f37df15550'
+export const ProxyAdmin = '0xcBB9cc3146d2b25d1E12E73214c17538246d2167'
+export const SWTRImplementation = '0x8e4E2ec57F183cdd99460c3A2e175Ef2B8Dff0d2'
+    
